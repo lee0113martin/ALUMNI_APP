2cf8aa5c658b3d9e90928186cd72de7e045d6699
lee0113martin <78179569+lee0113martin@users.noreply.github.com>
2024-09-29 17:27:37 -0700

    Add files via upload

--- alumni/urls.py
+++ alumni/urls.py
@@ -8,12 +8,15 @@ urlpatterns = [
     path('alumni/items/', views.items, name='items'),  
     path('alumni/leased_view/', views.leased_view, name='leased_view'),
     path('alumni/leased_view/leased_details/<int:id>', views.leased_details, name='leased_details'),
-    path('my_redirect', views.my_redirect, name='my_redirect'),  
+    #path('my_redirect', views.my_redirect, name='my_redirect'),  
 
 
 
     path('alumni/book_view/', views.book_view, name='book_view'),
     path('alumni/book_view/book_details/<int:id>', views.book_details, name='book_details'),
+    path('alumni/extra/', views.extra, name='extra'),
+
+    path('alumni/pdf_view/', views.pdf_view, name='pdf_view'),
 
 
 
--- alumni/views.py
+++ alumni/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import alumni_tb, dzolali, leased, books
 from django.shortcuts import redirect
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.http import FileResponse
+import os
+from django.conf import settings
+from django.http import Http404
+
 
 # A NEW VIEW(FUNCTION) TO RETRIEVE ALL RECORDS FROM THE TABLE
 def alumni_view(request):
@@ -56,8 +63,8 @@ def leased_details(request, id):
   return HttpResponse(template.render(context, request))
 
 # A NEW VIEW(FUNCTION) TO REDIRECT TO EXTERNAL LINK(LEARNERSPLATFORM.COM)
-def my_redirect(request):
-  return redirect("https://account.learnersplatform.com/login")
+#def my_redirect(request):
+ # return redirect("https://account.learnersplatform.com/login")
 
 
 # A NEW VIEW(FUNCTION) TO RETRIEVE ALL BOOKS FROM THE BOOKS TABLE
@@ -77,3 +84,49 @@ def book_details(request, id):
     'kamil': kamil,
   }
   return HttpResponse(template.render(context, request))
+
+  
+# A NEW VIEW(FUNCTION) TO HANDLE NAVIGATION SCHEME
+def my_navigation(request):
+    # Logic for your view
+    context = {}
+    referer = request.META.get('HTTP_REFERER')
+    if referer:
+        context['referer'] = referer
+    return render(request, 'details.html', context)
+
+
+# A NEW VIEW(FUNCTION) TO DISPLAY SIMPLE HTML PAGE CALLED KAMIL.HTML
+def extra(request):
+    return render(request, 'kamil.html')
+
+# A NEW VIEW(FUNCTION) TO DISPLAY PDF ON BUTTON CLICK
+def pdf_view(request):
+    filepath = os.path.join(settings.STATIC_ROOT, 'kana', 'alumni', 'static', 'RENEWAL.pdf')
+    if os.path.exists(filepath):
+        with open(filepath, 'rb') as pdf:
+            response = FileResponse(pdf, content_type='application/pdf')
+            response['Content-Disposition'] = 'inline; filename="RENEWAL.pdf"'
+            return response
+    else:
+        # Handle the error: file not found
+        from django.http import Http404
+        raise Http404("PDF file not found.")
+   
+  
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
